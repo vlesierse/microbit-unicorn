67600344e9e42785c1971618449528e79ecab082
Vincent Lesierse <vincent@lesierse.com>
2021-05-24 11:32:09 +0200

    Lotte: 5x5 Matrix

--- main.ts
+++ main.ts
@@ -5,18 +5,10 @@ basic.showNumber(4)
 basic.showNumber(5)
 basic.forever(function () {
     basic.showLeds(`
-        . . # . .
-        . . # . .
-        # . # . #
-        . # # # .
-        . . # . .
+        . . . # .
+        . . # . #
+        . . # # #
+        . . # . #
+        . . # # #
         `)
-    basic.showString("Hello!")
-    basic.showIcon(IconNames.Heart)
-    basic.showArrow(ArrowNames.North)
-    basic.showArrow(ArrowNames.East)
-    basic.showArrow(ArrowNames.South)
-    basic.showArrow(ArrowNames.West)
-    basic.clearScreen()
-    basic.pause(500)
 })
