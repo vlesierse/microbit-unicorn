ced29f45714956e1f4fb6c9bc60551952b2d73bd
Vincent Lesierse <vincent@lesierse.com>
2021-05-24 11:27:09 +0200

    5x5 Matrix

--- main.ts
+++ main.ts
@@ -1,10 +1,22 @@
-led.enable(true)
+basic.showNumber(1)
+basic.showNumber(2)
+basic.showNumber(3)
+basic.showNumber(4)
+basic.showNumber(5)
 basic.forever(function () {
-    led.toggle(1, 0)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # . # . #
+        . # # # .
+        . . # . .
+        `)
+    basic.showString("Hello!")
+    basic.showIcon(IconNames.Heart)
+    basic.showArrow(ArrowNames.North)
+    basic.showArrow(ArrowNames.East)
+    basic.showArrow(ArrowNames.South)
+    basic.showArrow(ArrowNames.West)
+    basic.clearScreen()
     basic.pause(500)
-    led.toggle(1, 0)
-    basic.pause(500)
-    led.plot(3, 4)
-    basic.pause(500)
-    led.unplot(3, 4)
 })
