1001aed645435df1b572e08cc7c65692ddf68078
Vincent Lesierse <vincent@lesierse.com>
2021-05-24 12:01:11 +0200

    Compas

--- main.ts
+++ main.ts
@@ -1,8 +1,70 @@
-led.enable(true)
+let x = 0
+input.calibrateCompass()
 basic.forever(function () {
-    if (input.temperature() > 29) {
-        basic.showIcon(IconNames.Heart)
+    x = input.compassHeading()
+    if (x >= 293 && x < 338) {
+        basic.showLeds(`
+            . . # # #
+            . . . # #
+            . . # . #
+            . # . . .
+            # . . . .
+            `)
+    } else if (x >= 23 && x < 68) {
+        basic.showLeds(`
+            # # # . .
+            # # . . .
+            # . # . .
+            . . . # .
+            . . . . #
+            `)
+    } else if (x >= 68 && x < 113) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (x >= 113 && x < 158) {
+        basic.showLeds(`
+            . . . . #
+            . . . # .
+            # . # . .
+            # # . . .
+            # # # . .
+            `)
+    } else if (x >= 158 && x < 203) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if (x >= 203 && x < 248) {
+        basic.showLeds(`
+            # . . . .
+            . # . . .
+            . . # . #
+            . . . # #
+            . . # # #
+            `)
+    } else if (x >= 248 && x < 293) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
     } else {
-        basic.showIcon(IconNames.SmallHeart)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
     }
 })
