c79256b9373edbd2c92afeb3987d3bcd6d68421b
Vincent Lesierse <vincent@lesierse.com>
2021-05-24 11:21:24 +0200

    Light Single LED

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+led.enable(true)
 basic.forever(function () {
-	
+    led.toggle(1, 0)
+    basic.pause(500)
+    led.toggle(1, 0)
+    basic.pause(500)
+    led.plot(3, 4)
+    basic.pause(500)
+    led.unplot(3, 4)
 })
